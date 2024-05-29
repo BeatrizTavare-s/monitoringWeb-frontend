a6e3b5a528a59851d186da290345bdec49e35812
Beatriz Tavares <beatriz98t@gmail.com>
2024-05-29 19:32:48 -0300

    feat: add changes to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MonitoringHistoryComponent } from './components/monitoring-history/monitoring-history.component';
+import { HttpClientModule } from '@angular/common/http';
+import { SiteFormComponent } from './components/site-form/site-form.component';
+import { FormsModule } from '@angular/forms';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MonitoringHistoryComponent,
+    SiteFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    MatTableModule,
+    MatPaginatorModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
